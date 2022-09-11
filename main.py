2fe3896cc78fabcda45e5d0dec985c9de497295b
greencrimea <tom.horton100@gmail.com>
2022-09-11 18:13:12 +1000

    .

--- main.py
+++ main.py
@@ -7,3 +7,11 @@ from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
 env = gym_super_mario_bros.make('SuperMarioBros-v0')
 env = JoypadSpace(env, SIMPLE_MOVEMENT)
 
+#run game and take random steps
+done = True
+for step in range(100000):
+    if done:
+        env.reset()
+    state, reward, done, info = env.step(env.action_space.sample())
+    env.render()
+env.close()
