991097c4b0d5ed1dbd9bc3d28a6a02e1f6bc4125
greencrimea <tom.horton100@gmail.com>
2022-09-11 19:13:52 +1000

    .

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import gym_super_mario_bros
 from nes_py.wrappers import JoypadSpace
 from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
+from gym.wrappers import FrameStack, GrayScaleObservation
+from stable_baseline3.common.vec_env import VecFrameStack, DummyVecEnv
+from matplotlib import pyplot as plt
 
 
 #setup game
@@ -8,6 +11,7 @@ env = gym_super_mario_bros.make('SuperMarioBros-v0')
 env = JoypadSpace(env, SIMPLE_MOVEMENT)
 
 #run game and take random steps
+'''
 done = True
 for step in range(100000):
     if done:
@@ -15,3 +19,10 @@ for step in range(100000):
     state, reward, done, info = env.step(env.action_space.sample())
     env.render()
 env.close()
+'''
+
+#wrap in grayscale
+env = GrayScaleObservation(env, keep_dim=True)
+
+
+
